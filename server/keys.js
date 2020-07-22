c5a1f58476e872b3f72f54004023c8004025c18f
Ivan <ivan@ivanaleksandrov.com>
2020-07-22 07:59:11 +0200

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+const { REDIS_HOST, REDIS_PORT, PGUSER, PGHOST, PGDATABASE, PGPASSWORD, PGPORT } = process.env;
+
+module.exports = {
+  redisHost: REDIS_HOST,
+  redisPort: REDIS_PORT,
+  pgUser: PGUSER,
+  phHost: PGHOST,
+  pgDatabase: PGDATABASE,
+  pgPassword: PGPASSWORD,
+  pgPort: PGPORT
+};
---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (isNaN(index)) {
+    throw new TypeError(`Expected argument to be of type Number, instead received ${typeof index}`);
+  }
+
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message, 10)));
+});
+
+sub.subscribe('insert');
